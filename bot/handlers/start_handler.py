610fc28b2f5b890fdabcd99c812a99d1e3933a49
JustGithubProject <Kropivadanil@gmail.com>
2024-01-29 18:56:38 +0200

    хендлеры в другом файле

--- bot/handlers/start_handler.py
+++ bot/handlers/start_handler.py
@@ -1,4 +1,22 @@
 ###############################
-# Обработчик команды /start.  #
+# Обработчик команды /start  #
 ###############################
 
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram import types
+
+# Объявляем клавиатуру
+keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+
+# Добавляем кнопки на клавиатуру
+buttons = [
+    KeyboardButton(text="Я предлагаю работу"),
+    KeyboardButton(text="Я ищу работу"),
+]
+
+# Добавляем кнопки на клавиатуру
+keyboard.add(*buttons)
+
+
+async def send_welcome(message: types.Message):
+    await message.answer("Привет! Выбери нужный тебе вариант!", reply_markup=keyboard)
--- bot/main.py
+++ bot/main.py
@@ -1,3 +1,22 @@
 ###################################################################################################################
 # Главный файл, запускающий бота. В этом файле будет код для создания и запуска бота, а также обработчиков событий#
 ###################################################################################################################
+import logging
+
+from aiogram import Bot
+from aiogram import Dispatcher
+from aiogram import executor
+
+from config import config_variables
+from handlers.start_handler import send_welcome
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=config_variables.TOKEN)
+
+dp = Dispatcher(bot)
+
+dp.register_message_handler(send_welcome, commands=['start'])
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
